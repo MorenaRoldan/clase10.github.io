d6967502b95eba0d7aad82182a177f0e353455c1
MorenaRoldan <167139119+MorenaRoldan@users.noreply.github.com>
2024-05-19 19:03:39 -0300

    Add files via upload

---
+++ script/script.js
@@ -0,0 +1,31 @@
+//var numero=4; //number
+//var nombre="morena";  //string
+//var boolean=true; //boolean
+//var nothing=null
+//var paises=["argentina","colombia","uruguay"];
+
+var numero_1=14;
+var numero_2=12;
+var sumaparcial=numero_1 + numero_2;
+
+console.log("la suma parcial es: " + sumaparcial)
+
+var numero_3=4;
+var sumatotal= sumaparcial + numero_3;
+console.log("la suma total es: " + sumatotal)
+
+alert("el resultado de la suma es : " + sumatotal)
+
+
+
+var nombre="morena"; //string
+var apellido="roldan"; //string
+console.log(nombre + "" + apellido);
+
+alert("el nombre completo es morena roldan")
+
+
+
+
+
+
